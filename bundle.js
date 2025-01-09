4a2a95a2ba6c78c322de59f31f142876505171f9
ddroid <contactahmadmunir@gmail.com>
2025-01-09 10:30:13 +0500

    Forget to build  v0.0.2

--- bundle.js
+++ bundle.js
@@ -1,26 +1,56 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const uiComponents = require('../src/index.js')
+module.exports.action_bar = require('action_bar');
+module.exports.graph_explorer = require('graph_explorer');
+module.exports.tabbed_editor = require('tabbed_editor');
+module.exports.chat_history = require('chat_history');
+},{"action_bar":2,"chat_history":3,"graph_explorer":4,"tabbed_editor":5}],2:[function(require,module,exports){
+module.exports = action_bar
 
-const x = uiComponents()
-
-document.body.append(x)
-
-},{"../src/index.js":2}],2:[function(require,module,exports){
-module.exports = uiComponents
-
-function uiComponents() {
-    const el = document.createElement('div') 
-    const shadow = el.attachShadow({ mode: 'closed' })
-    shadow.innerHTML = `
-    <h1>Ui Components for task-messenger</h1>
-    <p>These are some components that will be used in the task-messenger project.</p>
-    <ul>
-        <li>action bar</li>
-        <li>graph explorer</li>
-        <li>tabbed editor</li>
-        <li>chat history</li>
-    `
-    return el
+function action_bar() {
+    const element = document.createElement('div') 
+    const shadow = element.attachShadow({ mode: 'closed' })
+    shadow.innerHTML = `<h1>Action Bar....</h1>`
+    return element
 }
 
-},{}]},{},[1]);
+},{}],3:[function(require,module,exports){
+module.exports = chat_history
+
+function chat_history() {
+    const element = document.createElement('div') 
+    const shadow = element.attachShadow({ mode: 'closed' })
+    shadow.innerHTML = `<h1>Chat History....</h1>`
+    return element
+}
+
+},{}],4:[function(require,module,exports){
+module.exports = graph_explorer
+
+function graph_explorer() {
+    const element = document.createElement('div') 
+    const shadow = element.attachShadow({ mode: 'closed' })
+    shadow.innerHTML = `<h1>Graph Explorer....</h1>`
+    return element
+}
+
+},{}],5:[function(require,module,exports){
+module.exports = tabbed_editor
+
+function tabbed_editor() {
+    const element = document.createElement('div') 
+    const shadow = element.attachShadow({ mode: 'closed' })
+    shadow.innerHTML = `<h1>Tabbed Editor....</h1>`
+    return element
+}
+
+},{}],6:[function(require,module,exports){
+const components = require('..')
+
+const factories = Object.keys(components).map(name => [name, components[name]])
+
+document.body.append(...factories.map(([name, fn]) => {
+  const container = document.createElement('div')
+  container.append(fn())
+  return container;
+}))
+},{"..":1}]},{},[6]);
