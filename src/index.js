78a619fbf2375a3f76ae413ddafb048a48dbf090
ddroid <contactahmadmunir@gmail.com>
2025-01-15 22:08:46 +0500

    Action Wizard v0.0.1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports.action_bar = require('action_bar');
-module.exports.graph_explorer = require('graph_explorer');
-module.exports.tabbed_editor = require('tabbed_editor');
-module.exports.chat_history = require('chat_history');
+module.exports.action_bar = require("Action-Bar");
+// module.exports.graph_explorer = require("Graph-Explorer");
+// module.exports.tabbed_editor = require("Tabbed-Editor");
+// module.exports.chat_history = require("Chat-History");
--- web/index.js
+++ web/index.js
@@ -1,9 +1,14 @@
-const components = require('..')
+const components = require("..");
 
-const factories = Object.keys(components).map(name => [name, components[name]])
-
-document.body.append(...factories.map(([name, fn]) => {
-  const container = document.createElement('div')
-  container.append(fn())
-  return container;
-}))
+const factories = Object.keys(components).map((name) => [
+  name,
+  components[name],
+]);
+document.createElement("div").className = "root";
+document.body.append(
+  ...factories.map(([name, fn]) => {
+    const container = document.createElement("div");
+    container.append(fn());
+    return container;
+  })
+);
