7504aface6bd52cb607a337fc86ba455b8a47d3e
ddroid <contactahmadmunir@gmail.com>
2025-03-13 19:48:12 +0500

    commented state modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-module.exports.action_bar = require('action_bar/action')
+// module.exports.action_bar = require('action_bar/action')
 module.exports.chat_history = require('chat_history')
 module.exports.graph_explorer = require('graph_explorer')
 module.exports.tabbed_editor = require('tabbed_editor')
-module.exports.search_bar = require('search_bar')
+// module.exports.search_bar = require('search_bar')
