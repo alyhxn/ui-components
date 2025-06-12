9ca1e5b91c5c4cedc69316e7520c371c1b2a7452
alyhxn <hassanali8812@gmail.com>
2025-06-11 22:28:51 -0600

    Test

--- web/boot.js
+++ web/boot.js
@@ -1,6 +1,5 @@
-const hash = '6fc73361a2ea42065a15729b0974a1e5c10fa764'
-const prefix = 'https://raw.githubusercontent.com/alyhxn/playproject/' + hash + '/'
-const init_url = prefix + 'doc/state/example/init.js'
+const prefix = 'https://raw.githubusercontent.com/alyhxn/playproject/a31832ad3cb24fe15ab36bdc73a929f43179d7b8/'
+const init_url = location.hash === '#dev' ? 'web/init.js' : prefix + 'doc/state/example/init.js'
 const args = arguments
 
 fetch(init_url, { cache: 'no-store' }).then(res => res.text()).then(async source => {
@@ -9,5 +8,5 @@ fetch(init_url, { cache: 'no-store' }).then(res => res.text()).then(async source
   f(module, require)
   const init = module.exports
   await init(args, prefix)
-  require('./page')
+  require('./page') // or whatever is otherwise the main entry of our project
 })
